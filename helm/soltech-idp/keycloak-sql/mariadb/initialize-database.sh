29816855dde17bd052f129bdde22be43b41d99fb
Ray Elenteny <ray.elenteny@soltech.net>
2022-12-19 11:53:00 -0500

    MariaDB support

--- helm/soltech-idp/keycloak-sql/mariadb/initialize-database.sh
+++ helm/soltech-idp/keycloak-sql/mariadb/initialize-database.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
-echo "In MariaDB script."
+mariadb_host="${KEYCLOAK_DATABASE_HOST}"
+mariadb_database="${KEYCLOAK_DATABASE_DATABASE}"
+mariadb_username="${KEYCLOAK_DATABASE_USERNAME}"
+mariadb_password="${KEYCLOAK_DATABASE_PASSWORD}"
 
-# DROP USER IF EXISTS 'hutrics'@'%';
-# DROP DATABASE IF EXISTS hutrics;
+mysql -h ${mariadb_host} -u ${MARIADB_ROOT_USER} -p${MARIADB_ROOT_PASSWORD} -e "DROP USER IF EXISTS '${mariadb_username}'@'%';"
+mysql -h ${mariadb_host} -u ${MARIADB_ROOT_USER} -p${MARIADB_ROOT_PASSWORD} -e "DROP DATABASE IF EXISTS ${mariadb_database};"
 
-# CREATE USER 'hutrics'@'%' IDENTIFIED BY 'hutrics';
+mysql -h ${mariadb_host} -u ${MARIADB_ROOT_USER} -p${MARIADB_ROOT_PASSWORD} -e "CREATE USER '${mariadb_username}'@'%' IDENTIFIED BY '${mariadb_password}';"
 
-# CREATE DATABASE hutrics;
-# GRANT ALL PRIVILEGES ON hutrics.* TO 'hutrics'@'%';
+mysql -h ${mariadb_host} -u ${MARIADB_ROOT_USER} -p${MARIADB_ROOT_PASSWORD} -e "CREATE DATABASE ${mariadb_database};"
+mysql -h ${mariadb_host} -u ${MARIADB_ROOT_USER} -p${MARIADB_ROOT_PASSWORD} -e "GRANT ALL PRIVILEGES ON ${mariadb_database}.* TO '${mariadb_username}'@'%';"
+
+if [[ -f "./seed-database.sql" ]]
+then
+   mysql -h ${mariadb_host} -u ${mariadb_username} -p${mariadb_password} < ./seed-database.sql
+fi
